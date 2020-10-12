76949b603797e7992feea781948b42390d2a8492
II03uTuB <sir.med2012@gmail.com>
2020-10-12 18:11:23 +0300

    add start file for bot

---
+++ src/main/java/ru/medvedev/Application.java
@@ -0,0 +1,17 @@
+package ru.medvedev;
+
+import org.springframework.context.ApplicationContextInitializer;
+import org.telegram.telegrambots.ApiContextInitializer;
+
+import java.util.logging.Logger;
+
+public class Application {
+    private static final Logger LOGGER = Logger.getLogger(String.valueOf(Application.class));
+
+    public static void main(String[] args) {
+        ApiContextInitializer.init();
+        Bot news_bot = new Bot("orienteering_news_bot", "1228849064:AAG3H1x6tCM5lOki1FNrPDtmRTwKAGbUbB4");
+        news_bot.botConnect();
+    }
+
+}
