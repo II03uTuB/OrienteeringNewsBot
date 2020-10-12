021cbeffbc14ef38bd06a5658be2b5505e48654a
II03uTuB <sir.med2012@gmail.com>
2020-10-12 17:42:45 +0300

    Initial commit

---
+++ src/main/java/ru/medvedev/OrienteeringnewsbotApplication.java
@@ -0,0 +1,13 @@
+package ru.medvedev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrienteeringnewsbotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrienteeringnewsbotApplication.class, args);
+    }
+
+}
